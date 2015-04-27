3914ab1c8fe446d38315718363bd690fd778198d
sanctityQ <qicheng@tftiancai.com>
2015-04-27 18:19:45 +0800

    add check rule
    Change-Id: I0cbdd1fd13f2db65e6e1fd3274af306e6b7446c0

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,10 +1,16 @@
 printf '\033[0;34m%s\033[0m\n' "Upgrading flow"
 cd "$GITFLOW_DIR"
 
-require_clean_working_tree
-if git pull --rebase --stat origin master
-then
-  printf '\033[0;34m%s\033[0m\n' 'Hooray! flow has been updated and/or is at the current version.'
-else
-  printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
+
+git_compare_branches "master" "origin/master"
+
+local status=$?
+if [ $status -gt 0 ]; then
+    require_clean_working_tree
+    if git pull --rebase --stat origin master
+    then
+      printf '\033[0;34m%s\033[0m\n' 'Hooray! flow has been updated and/or is at the current version.'
+    else
+      printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
+    fi
 fi
